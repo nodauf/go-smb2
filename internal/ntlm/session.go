fefca042a5145289cac161bc26ac780172ba8967
Nodauf <nodauf@krutt.org>
2022-03-07 16:25:58 +0400

    Hopefully that one is good

--- internal/ntlm/session.go
+++ internal/ntlm/session.go
@@ -3,11 +3,10 @@ package ntlm
 import (
 	"bytes"
 	"crypto/rc4"
-	"encoding/binary"
 	"errors"
+	"unicode/utf16"
 
 	"github.com/nodauf/go-smb2/internal/utf16le"
-	"golang.org/x/sys/windows"
 )
 
 type Session struct {
@@ -167,15 +166,63 @@ func (s *Session) Unseal(dst, ciphertext []byte, seqNum uint32) ([]byte, uint32,
 // ref: http://davenport.sourceforge.net/ntlm.html#type2MessageExample
 func (s *Session) setTargetInfo(targetInfoEncoder *targetInfoEncoder) {
 	targetInfo := targetInfoEncoder.InfoMap
+
 	s.ntlmTargetInfoMap = map[string]string{
-		"ServerName":    windows.UTF16ToString(binary.BigEndian.Uint16(targetInfo[1])),
-		"DomainName":    windows.UTF16ToString(binary.BigEndian.Uint16(targetInfo[2])),
-		"DnsServerName": windows.UTF16ToString(binary.BigEndian.Uint16(targetInfo[3])),
-		"DnsDomainName": windows.UTF16ToString(binary.BigEndian.Uint16(targetInfo[4])),
+		"ServerName":    UTF16BytesToString(targetInfo[1]),
+		"DomainName":    UTF16BytesToString(targetInfo[2]),
+		"DnsServerName": UTF16BytesToString(targetInfo[3]),
+		"DnsDomainName": UTF16BytesToString(targetInfo[4]),
 	}
 }
+
+// Credits: https://github.com/elastic/go-windows/blob/main/utf16.go
 
 // NTLMTargetInfoMap returns a map[string]string of target information gathered from NTLMSSP negotiation
 func (s *Session) NTLMTargetInfoMap() map[string]string {
 	return s.ntlmTargetInfoMap
+}
+
+// indexNullTerminator returns the index of a null terminator within a buffer
+// containing UTF-16 encoded data. If the null terminator is not found -1 is
+// returned.
+func indexNullTerminator(b []byte) int {
+	if len(b) < 2 {
+		return -1
+	}
+
+	for i := 0; i < len(b); i += 2 {
+		if b[i] == 0 && b[i+1] == 0 {
+			return i
+		}
+	}
+
+	return -1
+}
+
+// UTF16BytesToString returns a string that is decoded from the UTF-16 bytes.
+// The byte slice must be of even length otherwise an error will be returned.
+// The integer returned is the offset to the start of the next string with
+// buffer if it exists, otherwise -1 is returned.
+func UTF16BytesToString(b []byte) string {
+	if len(b)%2 != 0 {
+		return "" //, 0, fmt.Errorf("Slice must have an even length (length=%d)", len(b))
+	}
+
+	/*offset := -1
+
+	// Find the null terminator if it exists and re-slice the b.
+	if nullIndex := indexNullTerminator(b); nullIndex > -1 {
+		if len(b) > nullIndex+2 {
+			offset = nullIndex + 2
+		}
+
+		b = b[:nullIndex]
+	}*/
+
+	s := make([]uint16, len(b)/2)
+	for i := range s {
+		s[i] = uint16(b[i*2]) + uint16(b[(i*2)+1])<<8
+	}
+
+	return string(utf16.Decode(s))
 }
